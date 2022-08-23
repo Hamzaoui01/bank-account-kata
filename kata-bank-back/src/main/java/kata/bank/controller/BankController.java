7e87cc052ebd5f780a5c4bbcadc990eb42ed1bfb
hamzaoui01 <hamzaoui.zakaria.1@gmail.com>
2022-08-23 00:02:58 +0000

    fix: return message from exception as response to http requests

--- kata-bank-back/src/main/java/kata/bank/controller/BankController.java
+++ kata-bank-back/src/main/java/kata/bank/controller/BankController.java
@@ -1,8 +1,7 @@
 package kata.bank.controller;
 
 import kata.bank.controller.dto.OperationDTO;
-import kata.bank.entity.Account;
-import kata.bank.repository.OperationRepository;
+import kata.bank.repository.AccountRepository;
 import kata.bank.service.BankService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,46 +10,56 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class BankController {
 
     private final BankService bankService;
-    private final OperationRepository operationRepository;
+    private final AccountRepository accountRepository;
 
     @GetMapping("accounts/{number}")
-    public ResponseEntity<Account> getAccount(@PathVariable String number){
+    public ResponseEntity getAccount(@PathVariable String number){
         try{
             return new ResponseEntity<>(bankService.getAccount(number),HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping("accounts")
+    public ResponseEntity getAccounts(){
+        try{
+            return new ResponseEntity<>(accountRepository.findAll(),HttpStatus.OK);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
     @PostMapping("operations/debit")
-    public ResponseEntity<Account> debitAccount(@RequestBody OperationDTO operationDTO){
+    public ResponseEntity debitAccount(@RequestBody OperationDTO operationDTO){
         if (operationDTO.getAmount()<=0) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         try{
             return new ResponseEntity<>(bankService.deposit(operationDTO.getAccountNumber(), operationDTO.getAmount()),HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
     @PostMapping("operations/credit")
-    public ResponseEntity<Account> creditAccount(@RequestBody OperationDTO operationDTO){
+    public ResponseEntity creditAccount(@RequestBody OperationDTO operationDTO){
         if (operationDTO.getAmount()<=0) return new ResponseEntity(HttpStatus.BAD_REQUEST);
         try{
             return new ResponseEntity<>(bankService.withDraw(operationDTO.getAccountNumber(), operationDTO.getAmount()),HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
     @GetMapping("operations/account/{number}")
-    public ResponseEntity<Account> getOperations(@PathVariable String number){
+    public ResponseEntity getOperations(@PathVariable String number){
         try{
             return new ResponseEntity<>(bankService.getAccount(number),HttpStatus.OK);
         }catch (Exception e){
-            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 
